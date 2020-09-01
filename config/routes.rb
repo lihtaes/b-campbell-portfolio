c2df72bb1a42daf44e5e6f17262ece01636aff75
Bradley Campbell <bb.campbell606@gmail.com>
2020-09-01 12:38:18 -0700

    url change

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get 'resume_item/:id', to: 'resume_items#show', as: 'resume_item_show'
 
-  get 'resume', to: 'pages#resume'
+  get 'experience', to: 'pages#resume'
   get 'contact', to: 'pages#contact'
   get 'tech-news', to: 'pages#tech_news'
   get 'about-me', to: 'pages#about'
